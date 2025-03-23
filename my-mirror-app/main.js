a86a4c5f153d852941c0709c95e68b20da616776
virtualis9 <peacockchristopher15@gmail.com>
2025-03-23 16:26:24 +0000

    added new files for the mirror app

--- my-mirror-app/main.js
+++ my-mirror-app/main.js
@@ -4,9 +4,16 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: false, // Disable node integration for security
+      contextIsolation: true,  // Helps to keep the app secure
+      enableWebSQL: true,      // Enable webSQL if needed for storage
+      experimentalFeatures: false, // Enable experimental web features if necessary
+    },
   });
 
   win.loadFile("index.html");
+  
 };
 
 app.whenReady().then(() => {
